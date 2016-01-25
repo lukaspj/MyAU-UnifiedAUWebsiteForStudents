865f8954173fc1356c026430f3358b4e7130e900
Lukas Joergensen <lukaspj@outlook.com>
2016-01-25 14:20:13 +0100

    Now reads Merit as well as Grades in StadsParser

--- framework/parsers/StadsResultsParser.php
+++ framework/parsers/StadsResultsParser.php
@@ -10,7 +10,8 @@ require_once __DIR__ . "/../Grade.php";
 
 class StadsResultsParser
 {
-    public static function ParseHTML($resultHtml) {
+    public static function ParseHTML($resultHtml)
+    {
         $dom = new DOMDocument;
         libxml_use_internal_errors(true);
         $dom->loadHTML($resultHtml);
@@ -18,22 +19,48 @@ class StadsResultsParser
         $xpath = new DOMXPath($dom);
         $resultRows = $xpath->query("//table[@id='resultTable']/tbody/*");
         $grades = array();
-        for($i = 0; $i < $resultRows->length; $i++) {
+        for ($i = 0; $i < $resultRows->length; $i++) {
             $row = $resultRows->item($i);
             $grades[] = new Grade(self::GetCourseName($row), self::GetCourseGrades($row), self::GetCourseECTS($row));
         }
-        return $grades;
+        $meritRows = $xpath->query("//table[@id='meritTable']/tbody/*");
+        $merits = array();
+        for ($i = 0; $i < $meritRows->length; $i++) {
+            $row = $meritRows->item($i);
+            $merits[] = new Grade(self::GetCourseNameForMerit($row),
+                self::GetCourseGradesForMerit($row),
+                self::GetCourseECTSForMerit($row));
+        }
+        return array($grades, $merits);
     }
 
-    private static function GetCourseName($row) {
-        return $row->childNodes->item(0)->textContent;
+    private static function GetCourseName($row)
+    {
+        return trim($row->childNodes->item(0)->textContent);
     }
 
-    private static function GetCourseGrades($row) {
-        return $row->childNodes->item(4)->textContent;
+    private static function GetCourseGrades($row)
+    {
+        return trim($row->childNodes->item(4)->textContent);
     }
 
-    private static function GetCourseECTS($row) {
-        return $row->childNodes->item(8)->textContent;
+    private static function GetCourseECTS($row)
+    {
+        return trim($row->childNodes->item(8)->textContent);
+    }
+
+    private static function GetCourseNameForMerit($row)
+    {
+        return trim($row->childNodes->item(0)->textContent);
+    }
+
+    private static function GetCourseGradesForMerit($row)
+    {
+        return trim($row->childNodes->item(6)->textContent);
+    }
+
+    private static function GetCourseECTSForMerit($row)
+    {
+        return trim($row->childNodes->item(10)->textContent);
     }
 }
